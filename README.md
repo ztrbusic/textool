532c91f04e25cc4c8b9d6a06688f02f1da642d94
Zeljko Trbusic <ztrbusic@gmail.com>
2025-11-26 19:18:58 +0100

    Update .gitignore

---
+++ README.md
@@ -0,0 +1,10 @@
+# TEXTOOL
+
+This tool was created as a learning project for regex and CLI implementation with typer. But it grew to be a somewhat useful tool for identifying personal identifiable information (PII) for Croatia.
+
+So far it can identify:
+- OIB (Osobni Identifikacijski Broj, Perosnal Identification Number)
+- JMBG (Jedinstveni Matični Broj Građana - Unique Master Citizen Number)
+- MBO (Matični Broj Osiguranika - Insured Person Number)
+
+This is version 0.0.1, more features to follow.
--- src/textool/extractors/pii_extractor.py
+++ src/textool/extractors/pii_extractor.py
@@ -20,8 +20,29 @@ def is_valid_jmbg(candidate_jmbg: str) -> bool:
 		K = 0
 	return K == jmbg_digits[12]
 
+def is_valid_mbo(candidate_mbo: str) -> bool:
+	mbo_digits = []
+	for d in candidate_mbo:
+		mbo_digits.append(int(d))
+	S = (
+		7 * mbo_digits[0]
+		+ 6 * mbo_digits[1]
+		+ 5 * mbo_digits[2]
+		+ 4 * mbo_digits[3]
+		+ 3 * mbo_digits[4]
+		+ 2 * mbo_digits[5]
+		+ 7 * mbo_digits[6]
+		+ 6 * mbo_digits[7]
+		)
+	K = 11 - (S % 11)
+	if K == 11:
+		K = 0
+	if K == 10:
+		return False
+	return K == mbo_digits[8]
+
 def scan_pii(path: Path) -> dict:
-	# TXT and PDF - reading the file
+# TXT and PDF - reading the file
 	try:
 		if path.suffix.lower() in [".txt"]:
 			try:
@@ -36,19 +57,22 @@ def scan_pii(path: Path) -> dict:
 			for page in reader.pages:
 				text += page.extract_text()
 
-		# OIB  - extraction and validation
+# OIB  - extraction and validation
 		candidates_oib = re.findall(r"\d{11}", text)
 		valid_oibs = []
 		for c in candidates_oib:
 			if oib.is_valid(c):
 				valid_oibs.append(c)
 
-		# JMBG - extraction and validation
+# JMBG - extraction and validation
 		candidates_jmbg = re.findall(r"\d{13}", text)
 		valid_jmbgs = []
 		for c in candidates_jmbg:
 			if is_valid_jmbg(c) == True:
-				valid_jmbgs.append(c)
+				dd = int(c[:2])
+				mm = int(c[2:4])
+				if 1 <= mm <= 12 and 1 <= dd <= 31:
+					valid_jmbgs.append(c)
 
 		return {"file": str(path), "status": "ok", "pii": {"OIBs": valid_oibs, "JMBGs": valid_jmbgs}}
 
